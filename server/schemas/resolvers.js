e12b6897bdebcd1e70706c4cb565e49a52383738
Preksha Gandhi <prekshagandhi@gmail.com>
2021-11-22 07:26:41 -0500

    Back-end refactoring done

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,59 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { body }) => {
+      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+
+      if (!user) {
+        throw new AuthenticationError("No User with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addBook: async (parent, { user, body }) => {
+      return User.findOneAndUpdate(
+        { _id: user._id },
+        {
+          $addToSet: { savedBooks: { body } },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removeBook: async (parent, { user, params }) => {
+      return User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
